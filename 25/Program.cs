b0bea1fa6e7f391523e0c53aaebb6d69868c7d23
Vitalas <Vitalas_2004@mail.ru>
2023-10-01 17:06:44 +0300

    add 25

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using System.Diagnostics.Contracts;
+
+Console.Write("Введите число A : ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B : ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB < 0) numberB = numberB * -1;
+
+int PowerA(int num)
+{
+
+    int powerA = numberA;
+
+    for (int i = 1; i < numberB; i++)
+    {
+        checked
+        {
+            powerA = numberA * powerA;
+        }
+
+    }
+    return powerA;
+}
+Console.WriteLine($"{numberA}, {numberB} -> {PowerA(numberA)}");
