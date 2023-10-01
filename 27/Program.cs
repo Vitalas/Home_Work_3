8705d22e3a08d910daf01da74858303cfdad69c0
Vitalas <Vitalas_2004@mail.ru>
2023-10-01 18:00:43 +0300

    add 27

--- 27/Program.cs
+++ 27/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number = number * -1;
+
+int SumDigit(int num)
+{
+    int sumDigit = 0;
+    for (int i = 0; num != 0; i++)
+    {
+        sumDigit = sumDigit + (num % 10);
+        num = num / 10;
+    }
+    return sumDigit;
+}
+
+Console.WriteLine($"{number} -> {SumDigit(number)}");
